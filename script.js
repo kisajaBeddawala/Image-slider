6e3345a18e122c5ec27af1f090005e4e014c4425
kisajaBeddawala <kisajab72@gmail.com>
2025-07-06 09:41:43 +0530

    bottom slider loop

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 const cards = document.querySelectorAll(".main-card");
 let currentIndex = 0;
 
+const bottomSlider = document.querySelector(".bottom-slider");
+const visibleCount = 5;
+const cardWidth = 170;
+let liveIndex = visibleCount;
 
+const originalCards = Array.from(bottomSlider.children);
+
+for(let i = 0; i < visibleCount; i++){
+    const firstClone = originalCards[i].cloneNode(true);
+    bottomSlider.append(firstClone);
+    const lastClone = originalCards[originalCards.length - i - 1].cloneNode(true);
+    bottomSlider.prepend(lastClone);
+}
+
+bottomSlider.style.transform = `translateX(-${liveIndex * cardWidth}px)`;
+
+function slideTo(index){
+    bottomSlider.style.transition = "transform 0.5s ease";
+    bottomSlider.style.transform = `translateX(-${index * cardWidth}px)`;
+    liveIndex = index;
+}
+
+bottomSlider.addEventListener("transitionend",()=>{
+    const totalCards = bottomSlider.children.length;
+
+    if(liveIndex >= totalCards - visibleCount){
+        bottomSlider.style.transition = "none";
+        liveIndex = visibleCount;
+        bottomSlider.style.transform = `translateX(-${liveIndex * cardWidth}px)`;
+    }
+
+    if(liveIndex < visibleCount){
+        bottomSlider.style.transition = "none";
+        liveIndex = totalCards - visibleCount * 2;
+        bottomSlider.style.transform = `translateX(-${liveIndex * cardWidth}px)`;
+    }
+})
 function showCard(index){
     cards.forEach(card => card.classList.remove("active"));
     cards[index].classList.add("active");
@@ -10,9 +46,11 @@ function showCard(index){
 document.getElementById("right").addEventListener("click",function(){
     currentIndex = (currentIndex + 1) % cards.length;
     showCard(currentIndex);
+    slideTo(liveIndex + 1);
 });
 
 document.getElementById("left").addEventListener("click",()=>{
     currentIndex = (currentIndex - 1 + cards.length) % cards.length;
     showCard(currentIndex);
+    slideTo(liveIndex - 1);
 })
